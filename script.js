22ef17ff03802e72cfe6e3ad768251c03ec19abf
Anna <anna.r.german1410@gmail.com>
2024-06-15 22:18:35 +0300

    feat: add function to mark code after clicking on button

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 window.addEventListener("load", start);
 
 function start() {    
+
     //проверяем, выведен ли на странице код HTML
     if(document.querySelector(".code-html") === null) {
         console.log("Код HTML отсутствует!"); 
-        
-        // получаем доступ к нужной кнопке, передаем переменную в функцию
         let btnHtml = document.querySelector("button.html");
         makeButtonDisabled(btnHtml); //вызов функции блокировки кнопки
 
     } else {
-        let codeHtml = document.querySelector(".code-html").innerText; //содержит сам текст кода
-        console.log(codeHtml);
+        // если код отображается, подсвечиваем код при клике на кнопку
+        let codeHtml = document.querySelector(".code-html"); //содержит код HTML
+        let btnHtml = document.querySelector(".stack-btn button.html"); // кнопка стека
+        btnHtml.addEventListener("click", function () { markCode(codeHtml, btnHtml) }); 
     }
 
     //проверяем, выведен ли на странице код CSS
     if(document.querySelector(".code-css") === null) {
         console.log("Код CSS отсутствует!");
-
         let btnCss = document.querySelector("button.css");
         makeButtonDisabled(btnCss); 
         
     } else {
         let codeCss = document.querySelector(".code-css");
-        console.log(codeCss);
+        let btnCss = document.querySelector(".stack-btn button.css");
+        btnCss.addEventListener("click", function () { markCode(codeCss, btnCss) });
     }
 
     //проверяем, выведен ли на странице код JS
     if(document.querySelector(".code-js") === null) {
         console.log("Код JS отсутствует!");
-
         let btnJs = document.querySelector("button.js");
         makeButtonDisabled(btnJs);
 
     } else {
         let codeJs = document.querySelector(".code-js");
-        console.log(codeJs);                                                                 
+        let btnJs = document.querySelector(".stack-btn button.js");
+        btnJs.addEventListener("click", function () { markCode(codeJs, btnJs) });
     }
 
 }
@@ -44,4 +45,17 @@ function start() {
 function makeButtonDisabled(button) {
     console.log("Делаем соответствующую кнопку неактивной!");
     button.setAttribute("disabled", "");
+}
+
+// подсвечивает код при клике на кнопку
+function markCode(code, button) {
+
+    console.log("Щелкнули на кнопку");
+    console.log(code); 
+    console.log(button); 
+    
+    code.style.backgroundColor = "rgb(22 22 22 / 91%)";
+    button.style.backgroundColor = "rgb(22 22 22 / 91%)";
+    button.style.color = "#bababa";
+
 }
